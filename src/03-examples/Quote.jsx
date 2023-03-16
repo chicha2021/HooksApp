c7f5cf15cf36f8876e5b211ea359d169d8a61dd0
chicha2021 <ezequielh.rafaela@gmail.com>
2023-03-16 12:36:46 -0300

    UseLayoutEffect modifications

--- src/03-examples/Quote.jsx
+++ src/03-examples/Quote.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import { useLayoutEffect, useRef, useState } from 'react';
 
-export const Quote = ( {name, species, image}) => {
+
+export const Quote = ({ name, species, image }) => {
+    const pRef = useRef();
+    const [boxSize, setBoxSize] = useState({ width: 0, height: 0 })
+    
+    useLayoutEffect(() => {
+        const { height, width } = pRef.current.getBoundingClientRect();
+        setBoxSize({ height, width });
+    
+    }, [ name ]);
+    
     return (
-        <blockquote className="blockquote text-center">
-            <p className="mb-1">{ name }</p>
-            <footer className="blackquote-footer">{ species }</footer>
-            <img src={ image } alt="Foto" />
-        </blockquote>
+        <>
+            <blockquote ref={ pRef } className="blockquote text-center">
+                <p className="mb-1">{ name }</p>
+                <footer className="blackquote-footer">{ species }</footer>
+                <img src={ image } alt="Foto" />
+            </blockquote>
+            <code>{ JSON.stringify(boxSize) }</code>
+        </>
     )
 };
+
